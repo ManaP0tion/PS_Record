c396f92b7c9ca25a8279e286d2c6d59aac4ae4b4
Aer0dev <45550827+Aer0dev@users.noreply.github.com>
2023-01-26 22:12:48 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, count;
+    cin >> count;
+    for(int i=0; i<count; i++){
+        int A, B;
+        cin >> A >> B;
+        cout << A+B << "\n";
+    }
+}
