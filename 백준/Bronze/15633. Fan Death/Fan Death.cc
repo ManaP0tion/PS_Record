41fd81f378b7e553dcca2164da4f93ff69251565
Aer0dev <45550827+Aer0dev@users.noreply.github.com>
2023-02-01 23:36:01 +0900

    [Bronze III] Title: Fan Death, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15633. Fan Death/Fan Death.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    int sum = 0;
+    cin >> num;
+    for(int i = 1; i<=num; i++){
+        if(num%i == 0){
+            sum+= i;
+        }
+    }
+    cout << sum*5 - 24;
+}
