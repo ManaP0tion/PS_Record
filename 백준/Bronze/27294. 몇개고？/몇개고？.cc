ac186c0a704b11970a0cc3b8cf90675f1f2f87cd
Aer0dev <45550827+Aer0dev@users.noreply.github.com>
2023-02-06 14:08:05 +0900

    [Bronze V] Title: 몇개고?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/27294. 몇개고？/몇개고？.cc
+++ 백준/Bronze/27294. 몇개고？/몇개고？.cc
@@ -1,16 +1,13 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main(){
-    int T, S;
-    cin >> T >> S;
-    if(T<=16 && T>=12){ 
-        if(S==1)        //점심시간이면서 술을 먹는 경우
-            cout << 280;
-        else if(S==0)   //점심시간이면서 술을 같이 먹지 않는 경우
-            cout << 320;
-    }
-    else{
-        cout << 280;
+int main() {
+    int t, s;
+    cin>>t>>s;
+
+    if(s) cout<<"280";
+    else {
+        if(t<=11 || t>16) cout<<"280";
+        else cout<<"320";
     }
 }
