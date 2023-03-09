cc0638afb5851b532476e167f79601f5ebb9e620
Aer0dev <45550827+Aer0dev@users.noreply.github.com>
2023-03-09 14:32:14 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string s;
+    int position;
+
+    
+    cin >> s;
+    cin >> position;
+
+    cout << s[position-1];
+}
