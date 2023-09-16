872f3ee0d2c54e0773199ae9e807593367c743cc
Aer0 <45550827+Aer0dev@users.noreply.github.com>
2023-09-16 23:04:13 +0900

    [Silver III] Title: 분수 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.cc
+++ 백준/Silver/1735. 분수 합/분수 합.cc
@@ -1,33 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int GCM(long long A, long long B) {
-	int temp = A % B;
-	if (temp == 0) {
-		return B;
-	}
-	else {
-		return GCM(B, temp);
-	}
-	
+int gcd(int a, int b){
+    if(a%b == 0){
+        return b;
+    }
+    else{
+        return gcd(b, a%b);
+    }
 }
 
-int main() {
-	std::cin.tie(nullptr);
-	std::ios_base::sync_with_stdio(false);
 
-	int A, B, C,D;
-	cin >> A >> B;
-	cin >> C >> D;
+int main(){
+    int u1, u2, d1, d2;
+    cin >> u1 >> d1 >> u2 >> d2;
+    
+    
+    int upper_final = (u1*d2) + (u2*d1);
+    int lower_final = d1*d2;
+    int g = gcd(upper_final, lower_final);
 
-	int ResultB = B * D;
-	int ResultA = (A * D) + (C * B);
-
-	int gcm = GCM(ResultA, ResultB);
-	ResultA /= gcm;
-	ResultB /= gcm;
-
-	cout << ResultA << " " << ResultB << "\n";
-
-	return 0;
+    upper_final /= g;
+    lower_final /= g;
+    cout << upper_final << " " << lower_final << "\n";
 }
