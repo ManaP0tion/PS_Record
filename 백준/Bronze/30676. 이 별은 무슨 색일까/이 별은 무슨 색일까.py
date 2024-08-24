d9c9bc1de21b0780ba00b72d5b3f8928ac150e51
ManaPotion <45550827+ManaP0tion@users.noreply.github.com>
2024-08-25 03:14:03 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,16 @@
+length = int(input())
+
+if length < 425:
+    print("Violet")
+elif length < 450:
+    print("Indigo")
+elif length < 495:
+    print("Blue")
+elif length < 570:
+    print("Green")
+elif length < 590:
+    print("Yellow")
+elif length < 620:
+    print("Orange")
+else:
+    print("Red")
