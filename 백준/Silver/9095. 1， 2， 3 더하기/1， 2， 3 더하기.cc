fafa2d1db6441b54b64f134eccbe2b2fc6381dbc
ManaPotion <45550827+ManaP0tion@users.noreply.github.com>
2024-09-03 15:23:51 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, temp;
+    cin >> n;
+    
+    int dp[12] = {0, };
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+
+    for(int i = 4; i< 12; i++){
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+    }
+    for(int i = 0; i<n; i++){
+        cin >> temp;
+        cout << dp[temp] << "\n";
+    }
+}
