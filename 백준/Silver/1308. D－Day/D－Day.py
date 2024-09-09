65d97bf65f66a543036ca6d8ce15447bb4d6a933
ManaPotion <45550827+ManaP0tion@users.noreply.github.com>
2024-09-09 13:11:50 +0900

    [Silver V] Title: D-Day, Time: 108 ms, Memory: 111488 KB -BaekjoonHub

--- 백준/Silver/1308. D－Day/D－Day.py
+++ 백준/Silver/1308. D－Day/D－Day.py
@@ -2,7 +2,23 @@ import datetime
 y1, m1, d1 = map(int, input().split())
 y2, m2, d2 = map(int, input().split())
 
-if datetime.date(y2, m2, d2) >= datetime.date(y1+1000, m1, d1):
-    print("gg")
+today = datetime.date(y1, m1, d1)
+dday = datetime.date(y2, m2, d2)
+
+diff = int((dday - today).days)
+
+flag = True
+
+if y2 - y1 > 1000:
+    flag = False
+elif y2 -y1 == 1000:
+    if d2 == d1:
+        if d2 >= d1:
+                flag = False
+    elif d2 > d1:
+        flag = False    
+        
+if flag == True:
+    print(f"D-{diff}")
 else:
-    print(f"D-{(datetime.date(y2, m2, d2) - datetime.date(y1, m1, d1)).days}")
+    print("gg")
