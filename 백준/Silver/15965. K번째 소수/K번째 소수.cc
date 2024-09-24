bb216a713e410c55f2ff9f21cad34f7a9dd8cc9f
ManaPotion <45550827+ManaP0tion@users.noreply.github.com>
2024-09-24 17:33:33 +0900

    [Silver II] Title: K번째 소수, Time: 16 ms, Memory: 4244 KB, Score: 25 point -BaekjoonHub

--- 백준/Silver/15965. K번째 소수/K번째 소수.cc
+++ 백준/Silver/15965. K번째 소수/K번째 소수.cc
@@ -1,15 +1,12 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(){
-    int num = 500001;
-    int arr[500002] ={0, };
-    int count = 0;
-
-    int input;
-    cin >> input;
-
+    int num = 500000;
+    int arr[500001] ={0, };
     arr[1] = 1;
+    vector <int> PrimeNumber;
     for(int i=2; i<=num; i++){
         for(int j=2; i*j<=num; j++){
             arr[i*j] = 1;
@@ -17,12 +14,12 @@ int main(){
     }
 
     for(int i=2; i<=num; i++){
-        if(arr[i] == 0){
-            if(count == input-1){
-                cout << i;
-                break;
-            }
-            count++;
-        }
+        if(arr[i] == 0)
+            PrimeNumber.push_back(i);
     }
+
+    int input;
+    cin >> input;
+
+    cout << PrimeNumber[input-1];
 }
