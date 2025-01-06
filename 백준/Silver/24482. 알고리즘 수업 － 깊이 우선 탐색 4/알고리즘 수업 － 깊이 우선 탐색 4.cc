fbe30b7a58e4cfa02df289281c5a7f709f5e4b3a
ManaPotion <45550827+ManaP0tion@users.noreply.github.com>
2025-01-06 23:36:21 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 4, Time: 204 ms, Memory: 12364 KB -BaekjoonHub

---
+++ 백준/Silver/24482. 알고리즘 수업 － 깊이 우선 탐색 4/알고리즘 수업 － 깊이 우선 탐색 4.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> graph;
+vector<bool> visited;
+vector<int> depth;
+
+void dfs(int node, int currDepth){
+    visited[node]= true;
+    depth[node] = currDepth;
+    
+
+    for(int next : graph[node])
+    {
+        if(!visited[next])
+            dfs(next, currDepth+1);
+    }
+}
+
+int main(){
+    int n, m, r;
+    cin >> n >> m >> r;
+
+    graph.resize(n+1);
+    visited.resize(n+1, false);
+    depth.resize(n+1, -1);
+
+    int u, v;
+    for(int i=0; i<m; i++){
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+
+    for(int i = 1; i <= n; i++)
+        sort(graph[i].begin(), graph[i].end(), greater<int>());
+
+    dfs(r, 0);
+
+    for(int i=1; i<=n; i++)
+        cout << depth[i] << "\n";
+}
