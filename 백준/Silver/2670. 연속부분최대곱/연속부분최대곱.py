b8dfb503ba3495c803252d6c80f20ed2c54bc749
ManaPotion <45550827+ManaP0tion@users.noreply.github.com>
2026-01-14 23:17:19 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+data = []
+for _ in range(n):
+    data.append(float(input()))
+    
+dp = [0 for _ in range(n)]
+dp[0] = data[0]
+
+for i in range(1, n):
+    dp[i] = max(dp[i-1]*data[i], data[i])
+    
+print('%0.3f' % max(dp))
