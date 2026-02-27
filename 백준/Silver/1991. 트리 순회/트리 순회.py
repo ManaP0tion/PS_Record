24c6d268c33742beb9fb3dd7aee140985ac54e72
ManaPotion <45550827+ManaP0tion@users.noreply.github.com>
2026-02-27 18:12:49 +0900

    [Silver I] Title: 트리 순회, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,38 @@
+N = int(input())
+edges = [ input().split() for _ in range(N)]
+
+tree = {}
+for edge in edges:
+    node = edge[0]
+    left = edge[1]
+    right = edge[2]
+    
+    tree[node] = [left, right]
+    
+    
+def preorder(node):
+    if node == '.':
+        return
+    print(node, end='')
+    preorder(tree[node][0])
+    preorder(tree[node][1])
+    
+def inorder(node):
+    if node == '.':
+        return
+    inorder(tree[node][0])
+    print(node, end='')
+    inorder(tree[node][1])
+
+def postorder(node):
+    if node == '.':
+        return
+    postorder(tree[node][0])
+    postorder(tree[node][1])
+    print(node, end='')
+    
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
