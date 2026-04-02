88714b798fcf0d95079834ec299bed1e655fc435
ManaPotion <45550827+ManaP0tion@users.noreply.github.com>
2026-04-02 16:26:27 +0900

    [Silver II] Title: 예산, Time: 100 ms, Memory: 110920 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+m = int(input())
+
+ans = 0
+start, end = 1, max(arr)
+
+while start<=end:
+    mid = (start + end) // 2
+    total = 0
+    
+    for i in arr:
+        if i > mid:
+            total += mid
+    
+        else:
+            total += i
+            
+    if total <= m:
+        ans = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+print(ans)
+        
