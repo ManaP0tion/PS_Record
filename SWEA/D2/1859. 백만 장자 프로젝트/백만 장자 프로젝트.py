4c9b8a9799a614c4b5fa34edf12d3df2248b1fd0
ManaPotion <45550827+ManaP0tion@users.noreply.github.com>
2026-04-18 16:11:57 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 753 ms, Memory: 200,944 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -1,14 +1,17 @@
-t = int(input())
-for i in range(t):
-    ans = 0
-    n = int(input())
-    arr = list(map(int, input().split()))
-    sellPrice = 0
+
+T = int(input())
+
+for testcase in range(1, T+1):
+    N = int(input())
+    arr = list(map(int, input().split())) # 배열 입력을 받는다
     
-    for val in arr[::-1]:
-        if val >= sellPrice:
-            sellPrice = val
-        else:
-            ans += sellPrice - val
+    sellPrice = 0    
+    ans = 0
+    
+    for val in arr[::-1]: # 거꾸로 순회하면서
+        if val >= sellPrice: # 해당가격이 sellPrice보다 크다면
+            sellPrice = val  # 가격 업데이트한다
+        ans += sellPrice - val # 현재값에 구매해서 최대값에 판매
         
-    print("#", i+1, " ", ans, sep="")
+    print(f"#{testcase} {ans}")
+            
