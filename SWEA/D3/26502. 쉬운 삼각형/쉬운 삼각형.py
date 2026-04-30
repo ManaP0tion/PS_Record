2cff5d18d29f427fc8051d3fd2f68258f1e41273
ManaPotion <45550827+ManaP0tion@users.noreply.github.com>
2026-04-30 15:30:25 +0900

    [D3] Title: 쉬운 삼각형, Time: 92 ms, Memory: 59,520 KB -BaekjoonHub

--- SWEA/D3/26502. 쉬운 삼각형/쉬운 삼각형.py
+++ SWEA/D3/26502. 쉬운 삼각형/쉬운 삼각형.py
@@ -1,24 +1,25 @@
-# 조건 : 한 변은 x축에 평행, 다른 한 변은 y축에 평행
-TC = int(input())
+T = int(input())
 
-for testcase in range(1, TC+1):
+for _ in range(T):
     N = int(input())
     points = [tuple(map(int, input().split())) for _ in range(N)]
+    
     maxArea = 0
     for i in range(N):
         x, y = points[i]
-        max_v = 0
+        max_w = 0
         max_h = 0
         
         for j in range(N):
             if i == j:
                 continue
-            tx, ty = points[j]
-            if tx == x:
-                max_v = max(max_v, abs(ty-y))
-            if ty == y:
-                max_h = max(max_h, abs(tx-x))
-        maxArea = max(maxArea, max_h * max_v)
+            cx, cy = points[j]
+            if cx == x:
+                max_h = max(max_h, abs(y - cy))
+            if cy == y:
+                max_w = max(max_w, abs(x-cx))
+            
+        maxArea = max(maxArea , max_w*max_h)
     print(maxArea)
-        
-        
+    
+    
