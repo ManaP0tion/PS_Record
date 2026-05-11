acd91f3007b8da8368734ddaf3f459833e268e7b
ManaPotion <45550827+ManaP0tion@users.noreply.github.com>
2026-05-11 14:52:53 +0900

    [D2] Title: 파리 퇴치, Time: 66 ms, Memory: 54,272 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,24 +1,17 @@
+T = int(input())
 
-
-def solve(arr,N, M):
-    maxKill = 0
-    for i in range(N-M+1):
+for testcase in range(1, T+1):
+    N, M = map(int, input().split())
+    
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    maxCNT = 0
+    for i in range (N-M+1):
         for j in range(N-M+1):
             cnt = 0
             for x in range(i, i+M):
                 for y in range(j, j+M):
                     cnt += arr[x][y]
-            maxKill = max(cnt, maxKill)
-    return maxKill
-
-
-
-T = int(input())
-
-for testcase in range(1, T+1):
-    N, M = map(int, input().split())
-    # N * N 의 배열
-    arr = [list(map(int, input().split())) for _ in range(N)]
-    ans =solve(arr,N, M)
-
-    print(f"#{testcase} {ans}")
+            maxCNT = max(maxCNT, cnt)
+        
+    print(f"#{testcase} {maxCNT}")
+            
